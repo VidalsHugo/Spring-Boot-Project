4bab03f829f1c87f95a4c37659d802c2d0fe0348
Hugo Vidal <vidalshugo1@gmail.com>
2024-02-10 12:58:16 -0300

    Criando validacao do Token jwt

--- src/main/java/com/devnews4/demo/services/TokenService.java
+++ src/main/java/com/devnews4/demo/services/TokenService.java
@@ -3,6 +3,7 @@ package com.devnews4.demo.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.devnews4.demo.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
 
@@ -25,6 +26,19 @@ public class TokenService {
             return token;
         }catch(JWTCreationException exception){
             throw new RuntimeException("Error while generating Token", exception);
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch(JWTVerificationException exception){
+            return "";
         }
     }
     private Instant genExpirationDate(){
