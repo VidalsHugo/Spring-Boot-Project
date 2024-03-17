662a21bcdfff297c10dd446ed681e8b1249b1d71
Hugo Vidal <vidalshugo1@gmail.com>
2024-03-17 18:06:02 -0300

    EndPoint retornando itens da API NYT

--- src/main/java/com/devnews4/demo/controllers/ApiNYTController.java
+++ src/main/java/com/devnews4/demo/controllers/ApiNYTController.java
@@ -1,5 +1,7 @@
 package com.devnews4.demo.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,15 +13,33 @@ import org.springframework.web.client.RestTemplate;
 public class ApiNYTController {
     @Value("${NYT_API_KEY}")
     private String nytKey;
+
     @GetMapping("/top-stories/technology")
     public String getTechnology() {
         try {
             String apiUrl = "https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=" + nytKey;
             RestTemplate restTemplate = new RestTemplate();
-            return restTemplate.getForObject(apiUrl, String.class);
+            String jsonResponse = restTemplate.getForObject(apiUrl, String.class);
+
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readTree(jsonResponse);
+            JsonNode resultsNode = rootNode.get("results");
+
+            // Extraindo apenas os 20 primeiros itens
+            StringBuilder limitedResponse = new StringBuilder("{\"results\":[");
+            int limit = Math.min(1, resultsNode.size());
+            for (int i = 0; i < limit; i++) {
+                limitedResponse.append(resultsNode.get(i).toString());
+                if (i < limit - 1) {
+                    limitedResponse.append(",");
+                }
+            }
+            limitedResponse.append("]}");
+
+            return limitedResponse.toString();
         } catch (Exception e) {
             return "Erro ao processar a solicitação";
         }
     }
-
 }
+
